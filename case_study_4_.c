9d02c443e65bc83706f9085bda20dc55bc56e178
avinash4775 <91842984+avinash4775@users.noreply.github.com>
2021-12-21 22:52:36 +0530

    Create case_study_4_.c

---
+++ case_study_4_.c
@@ -0,0 +1,29 @@
+/*
+A kid is going to his Nani house. He wants to carry some toys with him in a carry bag. But he can lift upto 5 kg weight in it. 
+Find out the maximum number of toys he can go with, to his nani’s house.
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[10],i,weight=0,num1=0,count=0;
+    printf("Enter the number of toys: ");
+    scanf("%d",&num1);
+    printf("Enter the weight of each toys in ascending order:");
+    for(i=0;i<num1;i++)
+    scanf("%d",&arr[i]);
+
+    
+    for(i=0;i<num1;i++){
+        if(weight<=5){
+            weight=weight+arr[i];
+            if(weight<=5)
+            count++;
+        }
+    }
+    printf("%d toys will be taken by the boy",count);
+
+    return 0;
+}
